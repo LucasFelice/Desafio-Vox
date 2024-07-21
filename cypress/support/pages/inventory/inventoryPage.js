c7f889c8fe798f575f3555e2286cf0c6048a2e21
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-21 15:38:40 -0300

    Subindo ajustes nos comando de inventoryPage inclusão 'addProductsByCart' e remoção dos demais comandos

--- cypress/support/pages/inventory/inventoryPage.js
+++ cypress/support/pages/inventory/inventoryPage.js
@@ -1,33 +1,11 @@
 const elements = require('./elements').INVENTORY;
 
 class inventoryPage{
-    
-    getTitleProduct(title) {
-        return this.elements.productTitle().should('have.text', title)
-    }
-    
-    getInventory(){
-        this.elements.inventoryGrid().should('be.visible');
-    }
 
-    setProduct(productName) {
-        this.elements.productByText(productName);
-    }
-
-    getProduct(productName) {
-        this.elements.productByText(productName).should('exist').and('contain.text', productName);
-    }
-
-    clickAddToCart(productName) {
-        this.elements.addToCartBtn(productName).click();
-    }
-
-    clickShoppingCart(){
-        this.elements.shoppingCartBtn().click();
-    }
-
-    validateUrl(expectedPart) {
-        this.elements.validateUrl(expectedPart);
+    addProductsByCart() {
+        cy.contains('button', 'Add to cart').click()
+        cy.get(elements.addBikeLightBtn).click()
+        cy.get(elements.shoppingCartBtn).click()
     }
 }
 
