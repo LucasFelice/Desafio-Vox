04330db5ee1e3288e0a6c2fc10500906aadd9866
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-21 17:12:33 -0300

    Subindo a inclusão do arquivo elements de checkout, pois foi desmembrado dos pageObjects referentes ao checkout 'Information, Overview e Finish'

---
+++ cypress/support/pages/checkout/elements.js
@@ -0,0 +1,10 @@
+export const CHECKOUT = {
+
+    firstNameInput: '#first-name',
+    lastNameInput: '#last-name',
+    postalCodeInput: '#postal-code',
+    continueBtn: '#continue',
+    finishBtn: '#finish',
+    thankYouLabel: '.complete-header'
+
+}
