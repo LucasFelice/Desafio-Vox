7a00d3e1ca8274757c63f06dcb104175a1877d9f
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-21 14:13:56 -0300

    Subindo ajustes na inventoryPage 'remoção do elements'

--- cypress/support/pages/inventory/inventoryPage.js
+++ cypress/support/pages/inventory/inventoryPage.js
@@ -1,20 +1,11 @@
+const elements = require('./elements').INVENTORY;
+
 class inventoryPage{
-
-    elements = {
-        productTitle: () => cy.get('.product_label'),
-        inventoryGrid: () => cy.get('#inventory_container'),
-        productByText: (productName) => cy.contains('.inventory_item_name', productName),
-        addToCartBtn: (productName) => cy.contains('.inventory_item', productName)
-            .find('button'),
-        shoppingCartBtn: () => cy.get('[data-icon="shopping-cart"]'),
-        validateUrl: (expectedPart) => cy.url().should('include', expectedPart)
-    }
-
+    
     getTitleProduct(title) {
         return this.elements.productTitle().should('have.text', title)
     }
     
-
     getInventory(){
         this.elements.inventoryGrid().should('be.visible');
     }
