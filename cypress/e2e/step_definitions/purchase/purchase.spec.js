9028694d080f5551f96a235fe7750af33031237b
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-21 17:04:44 -0300

    Subindo ajustes em todo o setup purchase com cucumber

--- cypress/e2e/step_definitions/purchase/purchase.spec.js
+++ cypress/e2e/step_definitions/purchase/purchase.spec.js
@@ -1,47 +1,38 @@
-import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps'
-import loginPage from '../pages/loginPage'
-import inventoryPage from '../pages/inventoryPage'
-import cartPage from '../pages/cartPage'
-import informationPage from '../pages/informationPage'
-import overviewPage from '../pages/overviewPage'
-import finishPage from '../pages/finishPage'
+import { Given, When, Then } from 'cypress-cucumber-preprocessor/steps'
+import login from '../../../support/pages/login/loginPage'
+import inventory from '../../../support/pages/inventory/inventoryPage'
+import information from '../../../support/pages/checkout/informationPage'
+const inventoryElements = require('../../../support/pages/inventory/elements').INVENTORY;
+const cartElements = require('../../../support/pages/cart/elements').CART;
+const checkoutElements = require('../../../support/pages/checkout/elements').CHECKOUT;
 
 Given("I valid the products in the shopping cart", () => {
-    cy.visit("https://www.saucedemo.com/v1/");
-    loginPage.typeUsername('standard_user');
-    loginPage.typePassword('secret_sauce');
-    loginPage.clickLogin();
-    inventoryPage.setProduct('Sauce Labs Backpack');
-    inventoryPage.setProduct('Sauce Labs Bike Light');
-    inventoryPage.clickAddToCart('Sauce Labs Backpack');
-    inventoryPage.clickAddToCart('Sauce Labs Bike Light');
-    inventoryPage.clickShoppingCart();
-    inventoryPage.getProduct('Sauce Labs Backpack');
-    inventoryPage.getProduct('Sauce Labs Bike Light');
+    cy.visit(Cypress.config().baseUrl)
+    login.doLogin('standard_user', 'secret_sauce')
+    inventory.addProductsByCart()
+    cy.get(inventoryElements.cartList).should('be.visible')
 })
 
 When("I click on the checkout button of the page cart", () => {
-    cartPage.clickCheckout();
-    informationPage.getTitleInformation('Checkout: Your Information');
+    cy.get(cartElements.checkoutBtn).click()
+    cy.contains('span', 'Checkout: Your Information').should('be.visible')
 })
 
 And("I type my first name, last name, and zip postal code in the information form", () => {
-    informationPage.typeFistName('Lucas');
-    informationPage.typeLastName('Desafio');
-    informationPage.typePostalCode('123456');
+    information.typeInformation('Lucas', 'Desafio', '123456')
 })
 
 And("I click on the continue button of the checkout page", () => {
-    informationPage.clickContinue();
+    cy.get(checkoutElements.continueBtn).click()
 
 }) 
 
 And("I valid the overview of the checkout page and click finish button", () => {
-    overviewPage.getTitleOverview('Checkout: Overview');
-    overviewPage.clickFinish();
+    cy.contains('span', 'Checkout: Overview').should('be.visible')
+    cy.get(checkoutElements.finishBtn).click()
 }) 
 
 Then("I have a successful purchase", () => {
-    finishPage.getTitleFinish('Finish');
-    finishPage.getThankYou('THANK YOU FOR YOUR ORDER');
+    cy.contains('span', 'Checkout: Complete!').should('be.visible')
+    
 })
