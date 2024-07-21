cf7f336aa32a48368f4646b4d104b462287d2a16
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-21 17:08:28 -0300

    Subindo ajustes na informatePage, pois existiam comandos que não estavam sendo utilizados

--- cypress/support/pages/checkout/informationPage.js
+++ cypress/support/pages/checkout/informationPage.js
@@ -1,33 +1,13 @@
+const elements = require('./elements').CHECKOUT;
+
 class informationPage{
 
-    elements = {
-        informationTitle: () => cy.get('.subheader'),
-        firstNameInput: () => cy.get('#first-name'),
-        lastNameInput: () => cy.get('#last-name'),
-        postalCodeInput: () => cy.get('#postal-code'),
-        continueBtn: () => cy.get('input[value="CONTINUE"]'),
-
+    typeInformation(firstName, lastName, postalCode){
+        cy.get(elements.firstNameInput).type(firstName)
+        cy.get(elements.lastNameInput).type(lastName)
+        cy.get(elements.postalCodeInput).type(postalCode)
     }
-
-    getTitleInformation(title) {
-        return this.elements.informationTitle().should('have.text', title)
-    }
-
-    typeFistName(fistName){
-        this.elements.firstNameInput().type(fistName);
-    }
-
-    typeLastName(lastName){
-        this.elements.lastNameInput().type(lastName);
-    }
-
-    typePostalCode(zipPostalCode){
-        this.elements.postalCodeInput().type(zipPostalCode);
-    }
-
-    clickContinue(){
-        this.elements.continueBtn().click();
-    }    
+    
 }
 
 module.exports = new informationPage();
