78b6a6846477742fc152a98b65f5234385ce2c5a
Lucas de Felice <lucasfelice92@gmail.com>
2024-07-22 08:56:33 -0300

    Subindo ajuste no cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,17 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
-const {defineConfig} = require("cypress");
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   chromeWebSecurity: false,
   e2e: {
     baseUrl: "https://www.saucedemo.com",
+    specPattern: "cypress/e2e/step_definitions/*.feature",
     setupNodeEvents(on, config) {
       on('file:preprocessor', cucumber())
+      return config;
     },
-    specPattern: "cypress/e2e/step_definitions/*.feature",
-    }
+    env: {
+      
+    },
+  }
 })
